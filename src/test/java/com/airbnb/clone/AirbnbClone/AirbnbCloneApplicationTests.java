fe5645f2c486723bec292d735497718876c1d7e6
pagarciaSim <pablo.garciasimavilla@gmail.com>
2025-04-04 17:30:34 +0200

    Init commit

---
+++ src/test/java/com/airbnb/clone/AirbnbClone/AirbnbCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airbnb.clone.AirbnbClone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirbnbCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
